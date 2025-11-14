80e9486d331719de2def25556c9cd7a80b0864ec
123amir <safaei.a1@gmail.com>
2025-11-14 21:04:44 +0100

    Added a customizable initial waittime for shock wave spawners

--- Scripts/shock_wave_spawner.gd
+++ Scripts/shock_wave_spawner.gd
@@ -1,15 +1,27 @@
 extends StaticBody3D
 
+
+@export var initial_wait_time: float = 0.0
 # should be changed to preload perhaps
 @export var shock_wave_scene: PackedScene
 
 func _ready() -> void:
-	spawn_shock_wave()
+	if initial_wait_time == 0.0:
+		spawn_shock_wave()
+	else:
+		$"Repating Shock Wave Timer".stop()
+		$"Initial Wait Timer".wait_time = initial_wait_time
+		$"Initial Wait Timer".start()
 
-func _on_timer_timeout() -> void:
+func _on_repating_shock_wave_timer_timeout() -> void:
 	spawn_shock_wave()
 
 func spawn_shock_wave():
 	var shock_wave: Shock_Wave = shock_wave_scene.instantiate()
 	add_child(shock_wave)
 	shock_wave.global_position = global_position
+
+
+func _on_initial_wait_timer_timeout() -> void:
+	spawn_shock_wave()
+	$"Repating Shock Wave Timer".start()
