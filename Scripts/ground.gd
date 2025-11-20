1389bfb9eb8633c215b4e558b0104d2568371efc
123amir <safaei.a1@gmail.com>
2025-11-21 00:54:51 +0100

    Ground measurements don't need to be even number anymore.
    If for example the width is uneven, the whole ground is shifted correctly to allign with the grid.

--- Scripts/ground.gd
+++ Scripts/ground.gd
@@ -30,10 +30,12 @@ enum Mode{
 @export var pre_color_extended: bool = true
 
 func _ready():
+	
 	if width % 2 == 1:
-		width += 1
+		global_position.x = 0.5
 	if length % 2 == 1:
-		length += 1
+		global_position.z = 0.5
+
 	
 	AudioManager.audio_tick.connect(change_tile_color)
 	
@@ -48,7 +50,6 @@ func _ready():
 	# Transformationsdaten setzen
 	var counter: int = 0
 	for x in range(width):
-		#for z in range(width):
 		for z in range(length):
 			var mesh_transform = Transform3D(Basis(), Vector3(x-(width)/2.0, -0.49, z-(length)/2.0))  # Setze Position
 			multimesh.set_instance_transform(counter, mesh_transform)
@@ -67,7 +68,7 @@ func _ready():
 	change_tile_color()
 	
 	$CollisionShape3D.shape.size = Vector3(width, 1.0, length)
-	$CollisionShape3D/MeshInstance3D.mesh.size = Vector2(width, length)
+	$MeshInstance3D.mesh.size = Vector2(width, length)
 
 func change_tile_color():
 	for i in range(width*length):
